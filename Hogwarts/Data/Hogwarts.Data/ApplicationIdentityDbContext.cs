1852097da6980ca89596655f7c98e2a7f6078876
Michael Shen <2439739932@qq.com>
2019-12-01 11:45:34 +0800

    Repository接口公共接口IBaseRepository以及公共实现BaseRepository

---
+++ Hogwarts/Data/Hogwarts.Data/ApplicationIdentityDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SPY.DB.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SPY.Data
+{
+    public class ApplicationIdentityDbContext: IdentityDbContext<ApplicationIdentityUser, ApplicationIdentityRole, string>
+    {
+        public ApplicationIdentityDbContext(DbContextOptions option):base(option)
+        {}
+    }
+}
