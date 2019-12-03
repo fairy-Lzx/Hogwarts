76062e4558b8d7e4764ac595d62285ef784354cf
Michael Shen <2439739932@qq.com>
2019-12-03 16:50:29 +0800

    改变仓库结构

---
+++ Data/Hogwarts.Data/ApplicationIdentityDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Hogwarts.DB.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hogwarts.Data
+{
+    public class ApplicationIdentityDbContext: IdentityDbContext<Teacher, Role, string>
+    {
+        public ApplicationIdentityDbContext(DbContextOptions option):base(option)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Teacher>(b =>
+            {
+                b.ToTable("Teacher");
+            });
+            builder.Entity<Role>(b =>
+            {
+                b.ToTable("IdentityRole");
+            });
+        }
+    }
+}
